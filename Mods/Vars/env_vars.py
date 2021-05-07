5a7d6c6f81759a8689bcfd344fed8b0c2ee42727
Gavin <gmcguigan@smarttech.com>
2021-05-07 19:16:54 +0200

    Fixed issue of changing profiles.  ENV_VARS are passed in with the variable file env_vars.py and NOT with the cmd line arg -v.  This is because I'm passing lists and dictionaries.

--- Mods/Vars/env_vars.py
+++ Mods/Vars/env_vars.py
@@ -1,4 +1,4 @@
-from Mods.AppGlobals.setup import LOGIT, CONFIG
+from Mods.AppGlobals.setup import LOGIT, get_config
 import sys 
 import os
 from time import time 
@@ -7,8 +7,8 @@ def get_variables():
     """ Takes in the app CFG_ENV_VARS and passes it to the robot api when launching tests. """
     # for k, v in GV.CFG_ENV_VARS.items():
     #     GV.CFG_ENV_VARS.setdefault(k, v)             # Should set the key value, only if it doesn't exist. 
-    for k, v in CONFIG.ENV_VARS.items():
+    for k, v in get_config().ENV_VARS.items():
         LOGIT.debug("-v  {}:{}".format(k, v))
     LOGIT.debug("")
 
-    return CONFIG.ENV_VARS
+    return get_config().ENV_VARS
