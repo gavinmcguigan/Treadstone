13e6d6e04347c74401799e2c3ca9342705f42f2a
Gavin <gmcguigan@smarttech.com>
2021-07-17 17:59:54 +0200

    Added the test library to the LumioLibrary

---
+++ Sandbox/Libs/LumioLibrary/TestLibs/__init__.py
@@ -0,0 +1,6 @@
+from .PurchaseForm import single_process_purchases
+from .PurchaseForm import multi_process_purchases
+from .Library import single_process_login
+from .Library import multi_process_login
+from .Library import pause_login
+from .Library import go_to_library
